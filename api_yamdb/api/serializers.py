037bef8a247de40f16fc6ec84c4016a080da4ecc
dmay92 <unic1992@yandex.ru>
2023-02-20 14:37:15 +0300

    iteration2 changes

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,64 +1,57 @@
-import datetime
 from rest_framework import serializers
-from reviews.models import (
-    Category, Genre,
-    Title,
-)
+
+from reviews.models import Category, Genre, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
     """Сериализатор категорий."""
+
     class Meta:
         model = Category
-        exclude = ('id',)
-        lookup_field = ('slug')
+        exclude = ("id",)
+        lookup_field = "slug"
 
 
 class GenreSerializer(serializers.ModelSerializer):
     """Сериализатор жанров."""
+
     class Meta:
         model = Genre
-        exclude = ('id',)
-        lookup_field = ('slug')
+        exclude = ("id",)
+        lookup_field = "slug"
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    """Сериализатор произведений при добавлении, изменении, удалении."""
+    """Сериализатор произведений основной."""
+
+    rating = serializers.FloatField(read_only=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            "id",
+            "name",
+            "year",
+            "rating",
+            "description",
+            "genre",
+            "category",
+        )
+
+
+class TitleReadSerializer(TitleSerializer):
+    """Сериализатор произведений чтение."""
+
+    genre = GenreSerializer(read_only=True, many=True)
+    category = CategorySerializer(read_only=True)
+
+
+class TitleWriteSerializer(TitleSerializer):
+    """Сериализатор произведений запись."""
+
     category = serializers.SlugRelatedField(
-        queryset=Category.objects.all(),
-        slug_field='slug'
+        slug_field="slug", queryset=Category.objects.all(), required=False
     )
     genre = serializers.SlugRelatedField(
-        queryset=Genre.objects.all(),
-        slug_field='slug',
-        many=True
+        slug_field="slug", queryset=Genre.objects.all(), many=True
     )
-
-    class Meta:
-        model = Title
-        fields = (
-            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
-        )
-
-    def validate_year(self, value):
-        year = datetime.date.today().year
-        if value > year:
-            raise serializers.ValidationError(
-                'Указанный год не может быть больше текущего!'
-            )
-        return value
-
-
-class TitleListSerializer(serializers.ModelSerializer):
-    """Сериализатора списка произведений."""
-    category = CategorySerializer(read_only=True)
-    genre = GenreSerializer(many=True, read_only=True)
-    rating = serializers.IntegerField(read_only=True)
-
-    class Meta:
-        model = Title
-        fields = (
-            'id', 'name', 'year', 'rating',
-            'description', 'genre', 'category'
-        )
-        read_only_fields = ('id', 'rating')
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,61 +1,63 @@
-from rest_framework import viewsets
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-from rest_framework import filters
-from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
+
 from reviews.models import Category, Comment, Genre, Review, Title
+
+from .filters import TitleFilter
 from .serializers import (
-    CategorySerializer, GenreSerializer, TitleSerializer, TitleListSerializer
+    CategorySerializer,
+    GenreSerializer,
+    TitleReadSerializer,
+    TitleWriteSerializer,
 )
 
 
 class CategorytViewSet(viewsets.ModelViewSet):
     """ViewSet для модели Category (Категории)."""
+
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filter_backends = (filters.SearchFilter, )
-    search_fields = ('name',)
-    lookup_field = ('slug')
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name",)
+    lookup_field = "slug"
 
     def get_object(self):
-        return get_object_or_404(
-            self.queryset, slug=self.kwargs["slug"])
+        return get_object_or_404(self.queryset, slug=self.kwargs["slug"])
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     """ViewSet для модели Genre (Жанры)."""
+
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    filter_backends = (filters.SearchFilter)
-    search_fields = ('name',)
-    filterset_fields = ('slug',)
-    lookup_field = ('slug')
-    ordering = ('slug',)
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
+    search_fields = ("name",)
+    filterset_fields = ("slug",)
+    lookup_field = "slug"
+    ordering = ("slug",)
 
     def get_object(self):
-        return get_object_or_404(
-            self.queryset, slug=self.kwargs["slug"])
+        return get_object_or_404(self.queryset, slug=self.kwargs["slug"])
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     """ViewSet для модели Title (Произведения)."""
-    queryset = Title.objects.all()
-    serializer_class = TitleListSerializer
 
-    def list(self, request):
-        queryset = self.filter_queryset(Title.objects.annotate(
-            rating=Avg('reviews__score')))
-        serializer = TitleListSerializer(queryset, many=True)
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            return self.get_paginated_response(serializer.data)
-        return Response(serializer.data)
+    queryset = (
+        Title.objects.all()
+        .annotate(Avg("reviews__score"))
+        .prefetch_related("category", "genre")
+    )
+    serializer_class = TitleReadSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
 
-    def retrieve(self, request, pk=None):
-        queryset = Title.objects.annotate(rating=Avg('reviews__score'))
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = TitleSerializer(user)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return TitleReadSerializer
+        return TitleWriteSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
