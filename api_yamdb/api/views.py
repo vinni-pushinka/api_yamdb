bcecaa757dba9bf741638afa0984e9a3b130b510
dmay92 <107582147+dmay92@users.noreply.github.com>
2023-02-20 21:37:48 +0300

    Revert "Add review comment"

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -14,7 +14,6 @@ from reviews.models import Category, Comment, Genre, Review, Title, User
 from api.permissions import IsAdminOrReadOnly, IsAuthorAdminModeratorOrReadOnly
 from api.serializers import UserSerializer, SignUpSerializer, ObtainTokenSerializer
 from api_yamdb.settings import EMAIL
-from rest_framework.pagination import LimitOffsetPagination
 
 from .filters import TitleFilter
 from .serializers import (
@@ -22,8 +21,6 @@ from .serializers import (
     GenreSerializer,
     TitleReadSerializer,
     TitleWriteSerializer,
-    CommentSerializer,
-    ReviewSerializer,
 )
 
 
@@ -140,53 +137,36 @@ class TitleViewSet(viewsets.ModelViewSet):
 
 class ReviewViewSet(viewsets.ModelViewSet):
     """ViewSet для модели Review (Отзыв)."""
-    # Указание сериализатора для валидации и сериализации
-    serializer_class = ReviewSerializer
-    # Ограничение доступа
-    permission_classes = ()
-    # Подключаем возможность пагинации
-    pagination_class = LimitOffsetPagination
 
-    def get_title(self):
-        """Получить Title (Произведение) по title_id."""
-        title_id = self.kwargs.get("title_id")
-        return get_object_or_404(Title, pk=title_id)
+    queryset = Review.objects.all()
 
-    def get_queryset(self):
-        """Получить набор Review (Отзыв) к Title (Произведение)."""
-        title = self.get_title()
-        return title.reviews.all()
+    # Добавить новый отзыв.
+    # Пользователь может оставить только один отзыв на произведение.
+    # Права доступа: **Аутентифицированные пользователи.**
 
-    def perform_create(self, serializer):
-        """Создать новый Review (Отзыв) к Title (Произведение)."""
-        serializer.save(
-            author=self.request.user,
-            title=self.get_title(),
-        )
+    # Получить отзыв по id для указанного произведения.
+    # Права доступа: **Доступно без токена.**
+
+    # Удалить отзыв по id
+    # Права доступа: **Автор отзыва, модератор или администратор.**
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     """ViewSet для модели Comment (Комментарий)."""
-    # Указание сериализатора для валидации и сериализации
-    serializer_class = CommentSerializer
-    # Ограничение доступа
-    permission_classes = ()
-    # Подключаем возможность пагинации
-    pagination_class = LimitOffsetPagination
 
-    def get_review(self):
-        """Получить Review (Отзыв) по review_id."""
-        review_id = self.kwargs.get("review_id")
-        return get_object_or_404(Review, pk=review_id)
+    queryset = Comment.objects.all()
 
-    def get_queryset(self):
-        """Получить набор Comment (Комментарий) к Review (Отзыву)."""
-        review = self.get_review()
-        return review.comments.all()
+    # Получить список всех комментариев к отзыву по id
+    # Права доступа: **Доступно без токена.**
 
-    def perform_create(self, serializer):
-        """Создать новый Comment (Комментарий) к Review (Отзыву)."""
-        serializer.save(
-            author=self.request.user,
-            review=self.get_review(),
-        )
+    # Добавить новый комментарий для отзыва.
+    # Права доступа: **Аутентифицированные пользователи.**
+
+    # Получить комментарий для отзыва по id.
+    # Права доступа: **Доступно без токена.**
+
+    # Частично обновить комментарий к отзыву по id.
+    # Права доступа: **Автор комментария, модератор или администратор**.
+
+    # Удалить комментарий к отзыву по id.
+    # Права доступа: **Автор комментария, модератор или администратор**.
