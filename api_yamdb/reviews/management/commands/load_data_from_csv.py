e0a7fafb8cb030a3ce96ea28e27795e041c694d3
dmay92 <unic1992@yandex.ru>
2023-02-22 04:30:46 +0300

    fix inport from csv

--- api_yamdb/reviews/management/commands/load_data_from_csv.py
+++ api_yamdb/reviews/management/commands/load_data_from_csv.py
@@ -13,28 +13,91 @@ from reviews.models import (
 )
 
 
-TABLES_DICT = {
-    User: "users.csv",
-    Category: "category.csv",
-    Genre: "genre.csv",
-    Title: "titles.csv",
-    Review: "review.csv",
-    Comment: "comments.csv",
-    GenreTitle: "genre_title.csv",
-}
+def obj_from_id(model, id):
+    return model.objects.get(pk=id)
 
 
 class Command(BaseCommand):
-    help = "Load data from csv files"
+    help = "load people from csv"
 
-    def handle(self, *args, **kwargs):
-        for model, base in TABLES_DICT.items():
-            with open(
-                f"{settings.BASE_DIR}/static/data/{base}",
-                "r",
-                encoding="utf-8",
-            ) as csv_file:
-                reader = DictReader(csv_file)
-                model.objects.bulk_create(model(**data) for data in reader)
-
-        self.stdout.write(self.style.SUCCESS("Successfully load data"))
+    def handle(self, *args, **options):
+        for row in DictReader(
+            open(f"{settings.BASE_DIR}/static/data/users.csv", encoding="utf8")
+        ):
+            users = User(
+                id=row["id"],
+                username=row["username"],
+                email=row["email"],
+                role=row["role"],
+                bio=row["bio"],
+                first_name=row["first_name"],
+                last_name=row["last_name"],
+            )
+            users.save()
+        for row in DictReader(
+            open(
+                f"{settings.BASE_DIR}/static/data/category.csv",
+                encoding="utf8",
+            )
+        ):
+            category = Category(
+                id=row["id"], name=row["name"], slug=row["slug"]
+            )
+            category.save()
+        for row in DictReader(
+            open(f"{settings.BASE_DIR}/static/data/genre.csv", encoding="utf8")
+        ):
+            genre = Genre(id=row["id"], name=row["name"], slug=row["slug"])
+            genre.save()
+        for row in DictReader(
+            open(
+                f"{settings.BASE_DIR}/static/data/titles.csv", encoding="utf8"
+            )
+        ):
+            titles = Title(
+                id=row["id"],
+                name=row["name"],
+                year=row["year"],
+                category=obj_from_id(Category, row["category"]),
+            )
+            titles.save()
+        for row in DictReader(
+            open(
+                f"{settings.BASE_DIR}/static/data/genre_title.csv",
+                encoding="utf8",
+            )
+        ):
+            genre_title = GenreTitle(
+                id=row["id"],
+                title=obj_from_id(Title, row["title_id"]),
+                genre=obj_from_id(Genre, row["genre_id"]),
+            )
+            genre_title.save()
+        for row in DictReader(
+            open(
+                f"{settings.BASE_DIR}/static/data/review.csv", encoding="utf8"
+            )
+        ):
+            review = Review(
+                id=row["id"],
+                title=obj_from_id(Title, row["title_id"]),
+                text=row["text"],
+                author=obj_from_id(User, row["author"]),
+                score=row["score"],
+                pub_date=row["pub_date"],
+            )
+            review.save()
+        for row in DictReader(
+            open(
+                f"{settings.BASE_DIR}/static/data/comments.csv",
+                encoding="utf8",
+            )
+        ):
+            comments = Comment(
+                id=row["id"],
+                review=obj_from_id(Review, row["review_id"]),
+                text=row["text"],
+                author=obj_from_id(User, row["author"]),
+                pub_date=row["pub_date"],
+            )
+            comments.save()
